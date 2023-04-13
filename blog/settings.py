d14ecd275031d5bad59bb51b3f8bd96b9d1e79e2
Esanov Ilnar <ilnaresanov2002@gmail.com>
2023-04-13 16:18:38 +0500

    added static

--- blog/settings.py
+++ blog/settings.py
@@ -122,7 +122,8 @@ import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / "static"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
